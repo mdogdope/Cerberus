7a8c447487d0602f5ff2adb99abef9fc6239b9ca
mdogdope <ekimhest@gmail.com>
2026-03-19 17:25:37 -0700

    Made class to handle vnc connection.

---
+++ screen_cap/vnc.py
@@ -0,0 +1,37 @@
+from vncdotool import api
+
+class VNCConnectionError(Exception):
+	pass
+
+class VNC:
+	def __init__(self, ip_address, port, password = None) -> None:
+		self.ip_address = ip_address
+		self.port = port
+		self.password = password
+		self.client = None
+		self.connect()
+	
+	def connect(self):
+		try:
+			if self.password:
+				self.client = api.connect(f"{self.ip_address}::{self.port}", timeout=10, password=self.password)
+			else:
+				self.client = api.connect(f"{self.ip_address}::{self.port}", timeout=10)
+		except Exception as e:
+			self.client = None
+			raise VNCConnectionError("Could not connect to VNC server") from e
+	
+	def get_screenshot(self):
+		if self.client is None:
+			raise VNCConnectionError("Not connected to VNC server")
+		
+		try:
+			self.client.refreshScreen()
+			return self.client.screen
+		except Exception as e:
+			self.client = None
+			raise VNCConnectionError("Failed to get screen") from e
+	
+	def reconnect(self):
+		self.client = None
+		self.connect()
